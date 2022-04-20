1d4382883357172f71339da6e564308c95a9283d
Lucre <josielindamaldonado@gmail.com>
2022-04-19 23:16:27 -0400

    dynamically added row in js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
 // DOM ELEMENTS
 
-
 // JS VARIABLES
-var currentDayEl = moment().format('dddd, MMMM Do');
+var currentDayEl = moment().format("dddd, MMMM Do");
 
 function displayDay() {
-    $('#currentDay').html(currentDayEl);
-    
+  $("#currentDay").html(currentDayEl);
+  displayPlanner();
 }
-displayDay()
+displayDay();
 
+function displayPlanner() {
+  $(".container").append(
+    "<div class='row'>" +
+      "<p class='col-1 hour time-block pt-3'>9AM</p>" +
+      "<input type='text' value='' class='col-10 description text-area' />" +
+      "<button class='col-1 saveBtn'>save</button>" +
+      "</div>"
+  );
+}
